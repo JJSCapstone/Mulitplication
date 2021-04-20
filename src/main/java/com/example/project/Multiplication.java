42ec6a003409d5a022fe54db3070b2f4a74839aa
jomoody10 <59669631+jomoody10@users.noreply.github.com>
2021-04-20 15:16:26 -0500

    Initial commit

---
+++ src/main/java/com/example/project/Multiplication.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+public class Multiplication
+{
+    
+    //Set values of these variables below. The value of A multiplied by B should equal C.
+    int A = 2;
+    int B = 4;
+    int C = 8;
+  
+    public Multiplication()
+    {
+
+    }
+
+ 
+    public int getA()
+    {
+        return A;
+    }
+    
+    public int getB()
+    {
+        return B;
+    }
+    
+    public int getC()
+    {
+        return C;
+    }
+}
